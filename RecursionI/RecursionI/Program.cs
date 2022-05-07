71d8b762b3aaf4e6a2ed49cdc5b39e53688b98ea
dranik <dranik111100rs@gmail.com>
2022-05-07 20:30:06 +0300

    Recursion I: Swap Nodes in Pairs

---
+++ RecursionI/RecursionI/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace RecursionI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SwapNodesInPairs();
+
+            Console.Read();
+        }
+
+        private static void SwapNodesInPairs()
+        {
+            var solution = new SwapNodesInPairs.Solution();
+
+            var listNode = new SwapNodesInPairs.ListNode
+            {
+                val = 1,
+                next = new SwapNodesInPairs.ListNode
+                {
+                    val = 2,
+                    next = new SwapNodesInPairs.ListNode
+                    {
+                        val = 3,
+                        next = new SwapNodesInPairs.ListNode
+                        {
+                            val = 4,
+                            next = new SwapNodesInPairs.ListNode
+                            {
+                                val = 5
+                            }
+                        }
+                    }
+                }
+            };
+
+            Console.Write($"{listNode.val} - {listNode.next.val} - {listNode.next.next.val} - {listNode.next.next.next.val} - {listNode.next.next.next.next.val}");
+
+            Console.WriteLine();
+
+            var res = solution.SwapPairs(listNode);
+
+            Console.Write($"{res.val} - {res.next.val} - {res.next.next.val} - {res.next.next.next.val} - {res.next.next.next.next.val}");
+        }
+    }
+}
---
+++ RecursionI/RecursionI/SwapNodesInPairs/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace RecursionI.SwapNodesInPairs
+{
+    public class Solution
+    {
+        public ListNode SwapPairs(ListNode node)
+        {
+            if (node == null || node.next == null)
+            {
+                return node;
+            }
+
+            var tmp = node.val;
+            node.val = node.next.val;
+            node.next.val = tmp;
+
+            SwapPairs(node.next.next);
+
+            return node;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
