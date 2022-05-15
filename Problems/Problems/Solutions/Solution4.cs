3e2acd808ef89e31d98218b8158f01635f3de2bf
dranik <dranik111100rs@gmail.com>
2022-05-15 17:10:40 +0300

    Problem 4. Median of Two Sorted Arrays

---
+++ Problems/Problems/Solutions/Solution4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Problems.Solutions
+{
+    /// <summary>
+    /// Median of Two Sorted Arrays
+    /// </summary>
+    public class Solution4 : ISolution
+    {
+        public void Exec()
+        {
+            Console.WriteLine(FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }));
+            Console.WriteLine(FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }));
+        }
+
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var workList = nums1.ToList();
+            workList.AddRange(nums2.ToList());
+            workList.Sort();
+
+            double result;
+            if (workList.Count() % 2 == 0)
+            {
+                result = (workList[(workList.Count() / 2) - 1] + workList[workList.Count() / 2]) / 2.0;
+            }
+            else
+            {
+                result = workList[((int)Math.Truncate((decimal)(workList.Count() / 2)))];
+            }
+
+            return result;
+        }
+    }
+}
