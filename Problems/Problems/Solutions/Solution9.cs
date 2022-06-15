8e967fba679201d5a457b60454a214db44c3c9f8
dranik <dranik111100rs@gmail.com>
2022-06-15 23:37:06 +0300

    Problem 9. Palindrome Number

---
+++ Problems/Problems/Solutions/Solution9.cs
@@ -0,0 +1,41 @@
+﻿namespace Problems.Solutions
+{
+    /// <summary>
+    /// Palindrome Number
+    /// </summary>
+    public class Solution9 : ISolution
+    {
+        public void Exec()
+        {
+            Helper.Output(IsPalindrome(121));
+            Helper.Output(IsPalindrome(983498732));
+            Helper.Output(IsPalindrome(0));
+            Helper.Output(IsPalindrome(-123));
+            Helper.Output(IsPalindrome(-121));
+        }
+
+        public bool IsPalindrome(int x)
+        {
+            return IsPalindrome(x.ToString());
+        }
+
+        private bool IsPalindrome(string value)
+        {
+            int left = 0;
+            int right = value.Length - 1;
+
+            while (left < right)
+            {
+                if (value[left] != value[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
